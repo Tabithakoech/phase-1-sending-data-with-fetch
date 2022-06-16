65e6bfb703547a3b3cec9ff2719f23e26f22b40f
TabithaKoech <tabbyk1718@gmail.com>
2022-06-16 15:15:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+
+    fetch('http://localhost:3000/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+        .then(res => res.json())
+        .then(data => addNewUser(data.id))
+        .catch(message => 'Unauthorized Access')
+
+
+}
+function addNewUser(user) {
+    document.querySelector('body').innerHTML = user
+    document.body.append(message)
+}
+
+
+
